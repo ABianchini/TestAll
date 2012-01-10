3708711f9b39b68e2a79f3a2ff86803eb4e9ee5a
Adriano Bianchini2 <animationbureau@gmail.com>
2012-01-09 20:20:15 -0800

    Added Main Menu of the application

--- Test All/gen/com/advancementbureau/testall/R.java
+++ Test All/gen/com/advancementbureau/testall/R.java
@@ -10,15 +10,34 @@ package com.advancementbureau.testall;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background1=0x7f040001;
+        public static final int background2=0x7f040002;
+        public static final int text=0x7f040000;
+    }
+    public static final class dimen {
+        public static final int text_size=0x7f050000;
+        public static final int title_size=0x7f050001;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int divider=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int Button_ActionBar=0x7f070004;
+        public static final int ImageView_Divider=0x7f070002;
+        public static final int ScrollView_Main=0x7f070003;
+        public static final int TextView_Title=0x7f070000;
+        public static final int TextView_Welcome=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int version_number=0x7f040002;
+        public static final int action_bar=0x7f060004;
+        public static final int app_name=0x7f060001;
+        public static final int hello=0x7f060000;
+        public static final int version_number=0x7f060002;
+        public static final int welcome_text=0x7f060003;
     }
 }
