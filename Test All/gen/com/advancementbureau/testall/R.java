4cdb06720c907e83ec426309ac0f6dfd9884a7d1
Adriano Bianchini2 <animationbureau@gmail.com>
2012-01-09 21:34:55 -0800

    Created the Changelog which wasn't working. I don't remember how I fixed it, but when I remember I will record it here.

--- Test All/gen/com/advancementbureau/testall/R.java
+++ Test All/gen/com/advancementbureau/testall/R.java
@@ -11,33 +11,50 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int background1=0x7f040001;
-        public static final int background2=0x7f040002;
-        public static final int text=0x7f040000;
+        public static final int background1=0x7f050001;
+        public static final int background2=0x7f050002;
+        public static final int menu_color=0x7f050003;
+        public static final int menu_glow=0x7f050004;
+        public static final int text=0x7f050000;
     }
     public static final class dimen {
-        public static final int text_size=0x7f050000;
-        public static final int title_size=0x7f050001;
+        public static final int text_size=0x7f060000;
+        public static final int title_size=0x7f060001;
     }
     public static final class drawable {
         public static final int divider=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int Button_ActionBar=0x7f070004;
-        public static final int ImageView_Divider=0x7f070002;
-        public static final int ScrollView_Main=0x7f070003;
-        public static final int TextView_Title=0x7f070000;
-        public static final int TextView_Welcome=0x7f070001;
+        public static final int Button_ActionBar=0x7f090007;
+        public static final int ImageView_Divider=0x7f090005;
+        public static final int ScrollViewSettings=0x7f090001;
+        public static final int ScrollView_Main=0x7f090006;
+        public static final int TextView_Change01=0x7f090000;
+        public static final int TextView_ChangeText=0x7f090002;
+        public static final int TextView_Title=0x7f090003;
+        public static final int TextView_Welcome=0x7f090004;
+        public static final int changelog_menu_item=0x7f090009;
+        public static final int settings_menu_item=0x7f090008;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int change=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int settings=0x7f030002;
+    }
+    public static final class menu {
+        public static final int gameoptions=0x7f080000;
+    }
+    public static final class raw {
+        public static final int changelog=0x7f040000;
     }
     public static final class string {
-        public static final int action_bar=0x7f060004;
-        public static final int app_name=0x7f060001;
-        public static final int hello=0x7f060000;
-        public static final int version_number=0x7f060002;
-        public static final int welcome_text=0x7f060003;
+        public static final int action_bar=0x7f070004;
+        public static final int app_name=0x7f070001;
+        public static final int changelog=0x7f070005;
+        public static final int hello=0x7f070000;
+        public static final int settings=0x7f070006;
+        public static final int version_number=0x7f070002;
+        public static final int welcome_text=0x7f070003;
     }
 }
--- Test All/src/com/advancementbureau/testall/TestAllActivity.java
+++ Test All/src/com/advancementbureau/testall/TestAllActivity.java
@@ -1,7 +1,10 @@
 package com.advancementbureau.testall;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class TestAllActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,19 @@ public class TestAllActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	getMenuInflater().inflate(R.menu.gameoptions, menu);
+    	menu.findItem(R.id.settings_menu_item).setIntent(new Intent(this, SettingsActivity.class));
+    	menu.findItem(R.id.changelog_menu_item).setIntent(new Intent(this, ChangeLogActivity.class));
+    	return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	super.onOptionsItemSelected(item);
+    	startActivity(item.getIntent());
+    	return true;
     }
 }
