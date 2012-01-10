9efc896a11400ce8b1289f4c31f80eee2da292ab
Servite Bianchini <animationbureau@gmail.com>
2012-01-10 11:36:13 -0800

    Updated changelog and version numbers

--- Test All/gen/com/advancementbureau/testall/R.java
+++ Test All/gen/com/advancementbureau/testall/R.java
@@ -26,16 +26,17 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int Button_ActionBar=0x7f090007;
-        public static final int ImageView_Divider=0x7f090005;
-        public static final int ScrollViewSettings=0x7f090001;
-        public static final int ScrollView_Main=0x7f090006;
-        public static final int TextView_Change01=0x7f090000;
-        public static final int TextView_ChangeText=0x7f090002;
-        public static final int TextView_Title=0x7f090003;
-        public static final int TextView_Welcome=0x7f090004;
-        public static final int changelog_menu_item=0x7f090009;
-        public static final int settings_menu_item=0x7f090008;
+        public static final int Button_ActionBar=0x7f090008;
+        public static final int Button_ActionBar_Back=0x7f090000;
+        public static final int ImageView_Divider=0x7f090006;
+        public static final int ScrollViewSettings=0x7f090002;
+        public static final int ScrollView_Main=0x7f090007;
+        public static final int TextView_Change01=0x7f090001;
+        public static final int TextView_ChangeText=0x7f090003;
+        public static final int TextView_Title=0x7f090004;
+        public static final int TextView_Welcome=0x7f090005;
+        public static final int changelog_menu_item=0x7f09000a;
+        public static final int settings_menu_item=0x7f090009;
     }
     public static final class layout {
         public static final int actionbar=0x7f030000;
