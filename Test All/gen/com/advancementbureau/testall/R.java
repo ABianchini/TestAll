33c6b64cc6c871b8aa0d7180ce1f9d61fb153202
Adriano Bianchini <animationbureau@gmail.com>
2012-01-14 08:58:29 -0800

    1.2.0 - Action Bar done. Notifications, Graphics, and Server work are next

--- Test All/gen/com/advancementbureau/testall/R.java
+++ Test All/gen/com/advancementbureau/testall/R.java
@@ -23,28 +23,27 @@ public final class R {
     }
     public static final class drawable {
         public static final int divider=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_test=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int Button_ActionBar=0x7f090009;
-        public static final int Button_ActionBar_Back=0x7f090000;
-        public static final int Button_Graphics=0x7f09000a;
-        public static final int Button_Graphics_Back=0x7f090004;
-        public static final int ImageView_Divider=0x7f090007;
-        public static final int ScrollViewSettings=0x7f090002;
-        public static final int ScrollView_Main=0x7f090008;
-        public static final int TextView_Change01=0x7f090001;
-        public static final int TextView_ChangeText=0x7f090003;
-        public static final int TextView_Title=0x7f090005;
-        public static final int TextView_Welcome=0x7f090006;
-        public static final int actionBarInfo_menu_item=0x7f09000b;
-        public static final int actionBar_derp=0x7f09000f;
-        public static final int actionBar_herp=0x7f09000e;
-        public static final int actionBar_save=0x7f09000d;
-        public static final int changelog_menu_item=0x7f090012;
-        public static final int graphicsAbout_menu_item=0x7f090011;
-        public static final int graphicsSettings_menu_item=0x7f090010;
-        public static final int settings_menu_item=0x7f09000c;
+        public static final int Button_ActionBar=0x7f090007;
+        public static final int Button_Graphics=0x7f090008;
+        public static final int ImageView_Divider=0x7f090005;
+        public static final int ScrollViewSettings=0x7f090001;
+        public static final int ScrollView_Main=0x7f090006;
+        public static final int TextView_Change01=0x7f090000;
+        public static final int TextView_ChangeText=0x7f090002;
+        public static final int TextView_Title=0x7f090003;
+        public static final int TextView_Welcome=0x7f090004;
+        public static final int actionBarInfo_menu_item=0x7f090009;
+        public static final int actionBar_derp=0x7f09000d;
+        public static final int actionBar_herp=0x7f09000c;
+        public static final int actionBar_save=0x7f09000b;
+        public static final int changelog_menu_item=0x7f090010;
+        public static final int graphicsAbout_menu_item=0x7f09000f;
+        public static final int graphicsSettings_menu_item=0x7f09000e;
+        public static final int settings_menu_item=0x7f09000a;
     }
     public static final class layout {
         public static final int actionbar=0x7f030000;
