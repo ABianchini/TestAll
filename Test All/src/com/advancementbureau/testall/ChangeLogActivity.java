674689493db0e0714ca0111817f7b455143bbdcd
Nikola Tesla <animationbureau@gmail.com>
2012-03-23 21:41:55 -0700

    Fixed "Action Bar Up" button in all activities

--- Test All/src/com/advancementbureau/testall/ChangeLogActivity.java
+++ Test All/src/com/advancementbureau/testall/ChangeLogActivity.java
@@ -4,8 +4,11 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class ChangeLogActivity extends Activity {
@@ -14,6 +17,8 @@ public class ChangeLogActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change);
+        ActionBar actionBar2 = getActionBar();
+        actionBar2.setDisplayHomeAsUpEnabled(true);
         
         InputStream iFile = getResources().openRawResource(R.raw.changelog);
         try {
@@ -35,5 +40,14 @@ public class ChangeLogActivity extends Activity {
     	dataIO.close();
     	is.close();
     	return sBuffer.toString();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	super.onOptionsItemSelected(item);
+		if (item.getItemId() == android.R.id.home) {
+			Intent intent2 = new Intent(this, TestAllActivity.class);
+			intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent2); }
+    	return true;
     }
 }
--- Test All/src/com/advancementbureau/testall/SettingsActivity.java
+++ Test All/src/com/advancementbureau/testall/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.advancementbureau.testall;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,6 +12,17 @@ public class SettingsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        ActionBar actionBar2 = getActionBar();
+        actionBar2.setDisplayHomeAsUpEnabled(true);
     }
     
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	super.onOptionsItemSelected(item);
+		if (item.getItemId() == android.R.id.home) {
+			Intent intent2 = new Intent(this, TestAllActivity.class);
+			intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent2); }
+    	return true;
+    }
 }
